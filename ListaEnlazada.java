742670598d56d6da0bf8843c042fd6a1b16d5109
Edith Lucero Vilcape ROsas <evilcape@unsa.edu.pe>
2022-06-05 23:25:55 -0500

    ejercicio1 ListaEnlazada

---
+++ ListaEnlazada.java
@@ -0,0 +1,52 @@
+public class ListaEnlazada 
+{
+    Nodo cabeza;
+
+    public ListaEnlazada()
+    {
+        cabeza=null;
+    }
+
+    public void insertar(String data)
+    {
+        Nodo aux=new Nodo(data);
+        
+        if(cabeza==null)
+        {
+            cabeza=aux;
+        }
+        else
+        {
+            Nodo tem=cabeza;
+            while(tem.next!=null)
+            {
+                tem=tem.next;
+            }
+            tem.next=aux;
+        }
+    }
+
+    public void recorrido()
+    {
+        Nodo temp=cabeza;
+        while(temp!=null)
+        {
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+    }
+
+    public boolean buscar(String data)
+    {
+        Nodo temp= cabeza;
+        while(temp!=null)
+        {
+            if(temp.data==data)
+            {
+                return true;
+            }
+            temp=temp.next;
+        }
+        return false;
+    }
+}
